a4d46abdb27ebbefe2385f1d91b8610823ec610b
Semert <semert2121@gmail.com>
2021-03-18 14:59:37 +0300

    MapContainer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
+import * as Data from "./test.json";
+import { MapContainer, TileLayer, LayersControl } from "react-leaflet";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  console.log("Test data", Data.default.features);
+
+  return (
+    <div className="App">
+      <MapContainer center={[41.00824, 28.978359]} zoom={12}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+};
 
 export default App;
