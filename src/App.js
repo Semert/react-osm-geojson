477c4fa5ebd1d5f2b5216482a7aabc84f0a24397
Semert <semert2121@gmail.com>
2021-03-18 15:00:53 +0300

    Dark mode for the map

--- src/App.js
+++ src/App.js
@@ -4,15 +4,36 @@ import * as Data from "./test.json";
 import { MapContainer, TileLayer, LayersControl } from "react-leaflet";
 
 const App = () => {
+  const { BaseLayer, Overlay } = LayersControl;
+
   console.log("Test data", Data.default.features);
 
   return (
     <div className="App">
-      <MapContainer center={[41.00824, 28.978359]} zoom={12}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+      <MapContainer
+        center={[41.00824, 28.978359]}
+        zoom={12}
+        doubleClickZoom={true}
+        animate={true}
+        duration={100}
+        bounceAtZoomLimits={true}
+        maxBoundsViscosity={0.95}
+        maxBounds={[
+          [-360, -180],
+          [360, 180],
+        ]}
+      >
+        <LayersControl position="topright">
+          <BaseLayer name="Normal">
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </BaseLayer>
+          <BaseLayer checked={true} name="Dark">
+            <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png" />
+          </BaseLayer>
+        </LayersControl>
       </MapContainer>
     </div>
   );
